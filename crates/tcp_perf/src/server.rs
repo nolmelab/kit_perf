ee9478419b2ce48ef8f270c7a2f99b85b22088e5
YongHyun Park <67228941+enkeejunior1@users.noreply.github.com>
2023-08-16 21:50:11 +0900

    ratatui와 tokio 공존 테스트

--- crates/tcp_perf/src/server.rs
+++ crates/tcp_perf/src/server.rs
@@ -1,14 +1,19 @@
 use std::sync::mpsc::{ self, Receiver, Sender };
-use std::thread;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::runtime::Builder;
 use anyhow;
+use ratatui::{backend::CrosstermBackend, widgets::Paragraph, Terminal};
+use std::{
+    thread,
+    io::stdout,
+    time::{Duration, Instant},
+};
 use crate::event::Event;
 
 pub fn run(args: super::Args) {
     let (tx, rx) = mpsc::channel::<Event>();
 
-    let runtime = Builder::new_multi_thread().thread_name("run-tcp").build().unwrap();
+    let runtime = Builder::new_multi_thread().enable_io().thread_name("run-tcp").build().unwrap();
 
     // 소유권을 자세히 잘 정리하면 안정성에 큰 도움이 된다. 그것이 러스트다. 
     let args2 = args.clone();
@@ -48,4 +53,35 @@ async fn run_stream(stream: TcpStream) -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-fn run_ui(args: &super::Args, rx: Receiver<Event>) {}
+fn run_ui(args: &super::Args, rx: Receiver<Event>) -> Result<(), anyhow::Error> {
+    let stdout = stdout();
+    // execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+
+    // Terminal은 buffers: [Buffer; 2]를 갖는다. 이중 버퍼링으로 보인다.
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    terminal.hide_cursor()?;
+
+    let mut running = true;
+    let mut message = "waiting...".to_string();
+    while running {
+        terminal.draw(|f| f.render_widget(Paragraph::new(message.as_str()), f.size()))?;
+
+        thread::sleep(Duration::from_millis(10));
+
+        let ev = rx.recv()?;
+        match ev {
+            Event::Accepted(addr) => {
+                message = format!("{:?}:{}", addr.ip(), addr.port());
+            },
+            _ => {
+
+            }
+        }
+    }
+
+    terminal.show_cursor()?;
+    terminal.flush()?;
+    Ok(())
+}
